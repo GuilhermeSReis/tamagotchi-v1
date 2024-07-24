6316fdf7d14534ea80f7e348c0043b698a83c656
Guilhermesdr <guilhermesdr13@gmail.com>
2024-07-24 16:10:14 -0300

    ✨ feat: Adicionando as view na controller

--- Tamagotchi/Controller/TamagotchiController.cs
+++ Tamagotchi/Controller/TamagotchiController.cs
@@ -15,6 +15,7 @@ namespace Tamagotchi.Controller
         private PokemonApiService pokemonApiService { get; set; }
         private List<PokemonResModel> pokemonsCadastrados{get;set;}
 
+        
 
         //Contrutor da Controller
         public TamagotchiController()
@@ -28,23 +29,62 @@ namespace Tamagotchi.Controller
         public void Start()
         {
             tamagotchiView.MensagemBoasVindas();
-            int opcao = 0;
+            string opcao ;
             do
             {
                 tamagotchiView.MenuInicial();
-                opcao = int.Parse(Console.ReadLine());
+                opcao = Console.ReadLine();
+
                 switch (opcao)
                 {
-                    case 1:
-                        //TODO: fazer parte de adoção
+                    case "1":
+                        tamagotchiView.MostrarPokemons(pokemonsCadastrados);
+                        string pokemonEscolhido = Console.ReadLine();
+                        if (pokemonsCadastrados.Any(x => x.Name == pokemonEscolhido))
+                        {
+                            var detalhePokemons = pokemonApiService.GetPokemonEscolhido(pokemonEscolhido);
+                            do
+                            {
+                                
+                                tamagotchiView.MenuPokemonEscolhido(detalhePokemons);
+                                opcao = Console.ReadLine();
+                                
+                                switch (opcao)
+                                {
+                                    case "1":
+                                    //TODO: fazer parte de adoção
+                                    break;
+                                    case "2":
+                                    //TODO: fazer parte das informações
+                                    break;
+                                    case "3":
+                                        Console.Write($"            Voltando");
+                                        foreach (var c in "......")
+                                        {
+                                            Console.Write(c);
+                                            Thread.Sleep(250);
+                                        }
+                                    break;
+                                    default:
+                                        Console.WriteLine($"            Opção ínvalida!!!");
+                                        Thread.Sleep(2000);
+                                    break;
+                                }
+                            } while (opcao != "3");
+                        }else
+                        {
+                            Console.WriteLine($"            Esse pokemon não existe ou não está cadastrado!!");
+                            Thread.Sleep(1000);
+                        }
+                        opcao = "";
                     break;
-                    case 2: 
+                    case "2": 
                         //TODO: fazer parte Interagir com seu Pokemon
                     break;
-                    case 3: 
+                    case "3": 
                         //TODO: fazer parte Ver Mascotes Adotados
                     break;
-                    case 4:
+                    case "4":
                         Console.Write($"            Saindo");
                         foreach (var c in "......")
                         {
@@ -58,7 +98,7 @@ namespace Tamagotchi.Controller
                         Thread.Sleep(2000);
                     break;
                 }                
-            } while (opcao != 4);
+            } while (opcao != "4");
         
         }
         
